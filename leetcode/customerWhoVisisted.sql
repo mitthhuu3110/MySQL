84135a3925126159594869a78baeeb4dfa6f8087
Mithilesh_cs <103479865+mitthhuu3110@users.noreply.github.com>
2023-06-26 22:34:20 +0530

    Create customerWhoVisisted.sql

---
+++ leetcode/customerWhoVisisted.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+SELECT customer_id, count(customer_id) as count_no_trans FROM Visits v LEFT JOIN Transactions t 
+ON v.visit_id = t.visit_id WHERE t.visit_id IS NULL GROUP BY customer_id;
