068fedb87a98141d20db5b561f41e2fb2675e941
Mithilesh_cs <103479865+mitthhuu3110@users.noreply.github.com>
2023-06-28 15:05:18 +0530

    Create projectEmployeesI.cpp

---
+++ leetcode/projectEmployeesI.cpp
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT project_id, ROUND(avg(experience_years), 2) AS average_years FROM Project AS p LEFT JOIN Employee AS e ON p.employee_id = e.employee_id GROUP BY project_id
